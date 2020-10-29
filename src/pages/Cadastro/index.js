1d054d229fa17741838a6d0d335b58b1cbd58280
Daltsz <gabrieldadalto@hotmail.com>
2020-10-29 10:16:55 -0300

    modificação do valor de retorno para o link

--- src/pages/Cadastro/index.js
+++ src/pages/Cadastro/index.js
@@ -69,7 +69,7 @@ export default function Cadastro() {
 
                 setTimeout(function(){
     
-                    window.location = 'https://projfinal-frontend.herokuapp.com/';
+                    window.location.assign("https://projfinal-frontend.herokuapp.com/");
                 
                 
                 },3000);
